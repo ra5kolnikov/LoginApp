8e2921e074406550529fe4e8f72fc338dd0e7fa0
UsoltsevVitaliy <usolcevvitaliy@gmail.com>
2022-06-08 15:20:57 +0700

    Add pragma marks and delete unnecessary comments

--- LoginApp/Controllers/AboutMeViewController.swift
+++ LoginApp/Controllers/AboutMeViewController.swift
@@ -13,7 +13,7 @@ class AboutMeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //MARK: Add Gradient for View Background
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [UIColor.gray.cgColor, UIColor.white.cgColor]
@@ -22,18 +22,5 @@ class AboutMeViewController: UIViewController {
         guard let  name = user.name else { return }
         guard let  surname = user.surname else { return }
         navigationItem.title = name + " " + surname
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
